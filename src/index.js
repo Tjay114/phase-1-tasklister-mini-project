f3b0cc2a567a67e2f098bd574daf2b2752d19a9f
Terormumuli <terahmumuli@gmail.com>
2022-06-23 09:16:40 +0300

    Tasklister

--- src/index.js
+++ src/index.js
@@ -1,24 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
 
-const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-  //ul where new tasks will live on the DOM
-  const taskUl = document.getElementById("tasks");
-  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
-  //attach event listeners
-  newTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    renderApp();
-  });
-  taskUl.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      taskList.deleteTask(e.target.dataset.description);
-      renderApp();
-    }
-  });
+  let form = document.querySelector('form')}
+	form.addEventListener('submit', (e) => {
+	  e.preventDefault()
+	  add(e.target['new-task-description'].value)
+	const form = document.querySelector("form");
+	form.addEventListener("submit", (event) => {
+	  event.preventDefault();
+	  const input = form.querySelector("#new-task-description").value;
+	  submitTodo(input)
+	  form.reset()
+	  })
+  }));
+
+  function add(input){
+	let li = document.createElement('li')
+	let btn = document.createElement('button')
+	btn.textContent = 'x'
+	btn.addEventListener('click', deleteList)
+	li.textContent = `${input}  `
+	document.querySelector('#list').appendChild(li)}
+  const submitTodo = (todo) => {
+	const ul = document.querySelector("#tasks")
+	const li = document.createElement("li");
+	const btn = document.createElement("button");
+	btn.addEventListener("click", deleteList)
+	btn.innerHTML = "<strong>DELETE</strong>";
+	li.textContent =`${todo} `;
+	li.appendChild(btn)
+	ul.appendChild(li);
+
+  }
+
+  function deleteList(e){
+	e.target.parentNode.remove()}
+  const deleteList = (event) => {
+	 event.target.parentNode.parentNode.remove();
+  }
+
+  // const newTaskForm = document.getElementById("create-task-form");
+  // const newTaskDescription = document.getElementById("new-task-description");
+  // // const newTaskPriority = document.getElementById("new-task-priority");
+  // const taskUl = document.getElementById("tasks");
+  // const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+  // newTaskForm.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   taskList.createNewTask(newTaskDescription.value);
+  //   e.target.reset();
+  //   renderApp();
+  // });
+  // taskUl.addEventListener("click", (e) => {
+  //   if (e.target.nodeName === "BUTTON") {
+  //     taskList.deleteTask(e.target.dataset.description);
+  //     renderApp();
+  //   }
+  // });
+
